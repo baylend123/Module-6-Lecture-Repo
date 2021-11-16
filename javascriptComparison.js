66209d50b38afa83089e3ce47ac64390ff5a4d29
baylend123 <baylendoss12@gmail.com>
2021-11-16 10:06:44 -0800

    added some lines to the JS comparison

--- javascriptComparison.js
+++ javascriptComparison.js
@@ -13,3 +13,31 @@ console.log("Comment inline") // Comment inline
 
 */
 
+// javascript version
+console.log({ gorilla: "large" } === { gorilla: "large" }) 
+// false
+
+// # python version
+// print({"gorilla": "large"} == {"gorilla": "large"}) 
+// # True
+
+// print(" ".join(["Hello", "World"])) # "Hello World"
+// # ["Hello", "World"].join(" ") JavaScript
+
+/*
+String Interpolation (Best)
+String formatting with f strings
+
+python version
+word = "hi"
+num = 6
+print(f"{word} and {num}")
+Most similar to interpolated strings in JavaScript:
+
+javascript version
+let word = "hi"
+let num = 6
+console.log(`${word} and ${num}`)
+
+*/ 
+
