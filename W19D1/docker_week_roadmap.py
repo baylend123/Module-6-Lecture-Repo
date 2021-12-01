5c026852c554aa392a494e2aa8e47a45e2babb4b
baylend123 <baylendoss12@gmail.com>
2021-12-01 13:05:16 -0800

    Docker day one notes

---
+++ W19D1/docker_week_roadmap.py
@@ -0,0 +1,22 @@
+'''
+
+Docker topics
+Setup and practice navigating inside a container (today)
+Containers, Images, & Dockerfiles (Thursday)
+Docker Compose (Friday)
+
+'''
+
+'''
+Docker containers (Thursday)
+A docker container is a lightweight package which 
+contains only the software and dependencies we need to 
+run a single process
+A container is a running image
+We’ll explore containers & use the command line
+interface to work with them
+'''
+
+'''
+
+'''
